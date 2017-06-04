76e2483e1aa28a4430a98b3da9a2d2a33136d664
YOOOOOOKI <zyqzyqzyq106@gmail.com>
2017-06-04 15:32:51 +0800

    第一次

--- main/main.js
+++ main/main.js
@@ -1,4 +1,43 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    
+	var arr0=[["._."],["|.|"],["|_|"]];
+	var arr1=[["..."],["..|"],["..|"]];
+
+	var arr2=[["._."],["._|"],["|_."]];
+	var arr3=[["._."],["._|"],["._|"]];
+
+	var arr4=[["..."],["|_|"],["..|"]];
+	var arr5=[["._."],["|_."],["._|"]];
+
+	var arr6=[["._."],["|_."],["|_|"]];
+	var arr7=[["._."],["..|"],["..|"]];
+
+	var arr8=[["._."],["|_|"],["|_|"]];
+	var arr9=[["._."],["|_|"],["..|"]];
+	var arr=[arr0,arr1,arr2,arr3,arr4,arr5,arr6,arr7,arr8,arr9];
+
+
+	var oldnum=[];
+	var realnum=parseInt(num);
+	while(realnum!==0)
+	{  
+		
+		oldnum.push(realnum%10);
+		
+      	realnum=parseInt(realnum/10);
+	}
+	for (var k = 0; k< 3; k++) {
+	for (var i = oldnum.length-1; i>=0; i--) {
+		if(i===0)
+		{console.log(arr[(oldnum[i])][k]);
+		}
+		else{
+			console.log(arr[(oldnum[i])][k]+" ");
+		}
+	}
+	console.log("<br>");
+	}
+	
+
+ 
 };
